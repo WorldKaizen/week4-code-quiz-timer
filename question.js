6322aaa6b50763d95bfa55a887878b6cb45f15d6
WorldKaizen <73084934+WorldKaizen@users.noreply.github.com>
2021-05-23 15:02:02 -0700

    Create question.js

---
+++ question.js
@@ -0,0 +1,29 @@
+var questions = [{
+	"question": "The first mechanical computer designed by Charles Babbage was called ?",
+	"option1": "Abacus",
+	"option2": "Analytical Engine",
+	"option3": "Calculator",
+	"option4": "Processor",
+	"answer": "2"
+}, {
+	"question": "Which of the following is the most powerful type of computer ?",
+	"option1": "Super-micro",
+	"option2": "Super conductor",
+	"option3": "Super computer",
+	"option4": "Megaframe",
+	"answer": "3"
+}, {
+	"question": "Which is a single integrated circuit?",
+	"option1": "Gate",
+	"option2": "Mother Board",
+	"option3": "Chip",
+	"option4": "CPU",
+	"answer": "1"
+}, {
+	"question": "C is ?",
+	"option1": "A third generation high level language",
+	"option2": "A machine language",
+	"option3": "An assembly language",
+	"option4": "All of the above",
+	"answer": "1"
+}]
